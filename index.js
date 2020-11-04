9407557580a3d9ab78f49c365b68a8db2626a5a3
Adriel dos Santos <adrieldossantos7@gmail.com>
2020-11-04 16:19:22 -0300

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+
+$('.project-content-articles').removeClass().addClass('hide')
+$('.project-content-notes').removeClass().addClass('hide')
+
+$('.project-content-myEnglish').click(()=>{
+    $('.project-content-myEnglish').removeClass().addClass('hide')
+})
+
+$('.project-content-articles').click(()=>{
+    $('.project-content-myEnglish').removeClass().addClass('hide')
+})
+
+$('.project-content-notes').click(()=>{
+    $('.project-content-myEnglish').removeClass().addClass('hide')
+})
