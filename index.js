03369af5e258074c1bf81fbcd80406569d91bdce
Adriel dos Santos <adrieldossantos7@gmail.com>
2020-11-06 00:59:46 -0300

    update

--- index.js
+++ index.js
@@ -35,3 +35,113 @@
  })
 
 
+//Tecnologias
+
+$('#node-content').addClass('hide')
+$('#angular-content').addClass('hide')
+$('#vue-content').addClass('hide')
+$('#php-content').addClass('hide')
+$('#BeJ-content').addClass('hide')
+$('#react-name').removeClass().addClass('project-name-opacity')
+
+
+$('#node').click(function(){
+    $('#node-content').removeClass().addClass('show')
+    $('#angular-content').removeClass().addClass('hide')
+    $('#vue-content').removeClass().addClass('hide')
+    $('#php-content').removeClass().addClass('hide')
+    $('#BeJ-content').removeClass().addClass('hide')
+    $('#react-content').removeClass().addClass('hide')
+
+    $('#node-name').removeClass().addClass('project-name-opacity')
+    $('#react-name').removeClass().addClass('project-name')
+    $('#angular-name').removeClass().addClass('project-name')
+    $('#vue-name').removeClass().addClass('project-name')
+    $('#php-name').removeClass().addClass('project-name')
+    $('#BeJ-name').removeClass().addClass('project-name')
+
+})
+
+$('#php').click(function(){
+    $('#node-content').removeClass().addClass('hide')
+    $('#angular-content').removeClass().addClass('hide')
+    $('#vue-content').removeClass().addClass('hide')
+    $('#php-content').removeClass().addClass('show')
+    $('#BeJ-content').removeClass().addClass('hide')
+    $('#react-content').removeClass().addClass('hide')
+
+    $('#node-name').removeClass().addClass('project-name')
+    $('#react-name').removeClass().addClass('project-name')
+    $('#angular-name').removeClass().addClass('project-name')
+    $('#vue-name').removeClass().addClass('project-name')
+    $('#php-name').removeClass().addClass('project-name-opacity')
+    $('#BeJ-name').removeClass().addClass('project-name')
+
+})
+
+$('#angular').click(function(){
+    $('#node-content').removeClass().addClass('hide')
+    $('#angular-content').removeClass().addClass('show')
+    $('#vue-content').removeClass().addClass('hide')
+    $('#php-content').removeClass().addClass('hide')
+    $('#BeJ-content').removeClass().addClass('hide')
+    $('#react-content').removeClass().addClass('hide')
+
+    
+    $('#node-name').removeClass().addClass('project-name')
+    $('#react-name').removeClass().addClass('project-name')
+    $('#angular-name').removeClass().addClass('project-name-opacity')
+    $('#vue-name').removeClass().addClass('project-name')
+    $('#php-name').removeClass().addClass('project-name')
+    $('#BeJ-name').removeClass().addClass('project-name')
+})
+
+$('#BeJ').click(function(){
+    $('#node-content').removeClass().addClass('hide')
+    $('#angular-content').removeClass().addClass('hide')
+    $('#vue-content').removeClass().addClass('hide')
+    $('#php-content').removeClass().addClass('hide')
+    $('#BeJ-content').removeClass().addClass('show')
+    $('#react-content').removeClass().addClass('hide')
+
+    $('#node-name').removeClass().addClass('project-name')
+    $('#react-name').removeClass().addClass('project-name')
+    $('#angular-name').removeClass().addClass('project-name')
+    $('#vue-name').removeClass().addClass('project-name')
+    $('#php-name').removeClass().addClass('project-name')
+    $('#BeJ-name').removeClass().addClass('project-name-opacity')
+})
+
+$('#vue').click(function(){
+    $('#node-content').removeClass().addClass('hide')
+    $('#angular-content').removeClass().addClass('hide')
+    $('#vue-content').removeClass().addClass('show')
+    $('#php-content').removeClass().addClass('hide')
+    $('#BeJ-content').removeClass().addClass('hide')
+    $('#react-content').removeClass().addClass('hide')
+
+    
+    $('#node-name').removeClass().addClass('project-name')
+    $('#react-name').removeClass().addClass('project-name')
+    $('#angular-name').removeClass().addClass('project-name')
+    $('#vue-name').removeClass().addClass('project-name-opacity')
+    $('#php-name').removeClass().addClass('project-name')
+    $('#BeJ-name').removeClass().addClass('project-name')
+})
+
+$('#react').click(function(){
+    $('#node-content').removeClass().addClass('hide')
+    $('#angular-content').removeClass().addClass('hide')
+    $('#vue-content').removeClass().addClass('hide')
+    $('#php-content').removeClass().addClass('hide')
+    $('#BeJ-content').removeClass().addClass('hide')
+    $('#react-content').removeClass().addClass('show')
+
+    $('#node-name').removeClass().addClass('project-name')
+    $('#react-name').removeClass().addClass('project-name-opacity')
+    $('#angular-name').removeClass().addClass('project-name')
+    $('#vue-name').removeClass().addClass('project-name')
+    $('#php-name').removeClass().addClass('project-name')
+    $('#BeJ-name').removeClass().addClass('project-name')
+})
+
